1e11b87a8db904abb1d7c67d4abf8a91b3c5aa6e
Mark Barbaric <markantonybarbaric@hotmail.com>
2024-03-16 14:42:05 +0000

    minor tidy up and started ordinal encoding.

--- disaster_tweets/lib/jupyter_helpers.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-def append_current_path():
-    current_path = os.getcwd()
-    print(f"current path {current_path}")
-    relative_path=os.path.dirname(current_path)
-    print(f"root path {relative_path}")
-    sys.path.append(f"{relative_path}")
